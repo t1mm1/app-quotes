078cd243c348ab36e7d2a18e67037981815e4fde
Pavel Kasianov <p.kasianov@gmail.com>
2025-07-22 23:02:41 +0300

    Updated env sample file

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,23 +1,21 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 /**
  * Sequelize instance for connecting to the PostgreSQL database.
  *
- * Database: 'db'
- * Username: 'admin'
- * Password: 'admin_password'
- * Host: 'localhost'
- * Port: 5432
- * Dialect: 'postgres'
- * Logging: false
- *
  * @type {import('sequelize').Sequelize}
  */
-const sequelize = new Sequelize('db', 'admin', 'admin_password', {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
