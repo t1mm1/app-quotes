78492e7a63850f22b2b8f1c9969b4cfa0a8205d4
Pavel Kasianov <p.kasianov@gmail.com>
2025-07-24 14:44:03 +0300

    Init and add client app

---
+++ client/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-start justify-left h-screen">
+      <h1 className="mt-2 p-4 text-2xl text-left">
+        Quotes Next.js application
+      </h1>
+    </div>
+  );
+}
