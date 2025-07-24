74f003a8f4a2e7735b52c72f3bbf2ffb0069287a
Pavel Kasianov <p.kasianov@gmail.com>
2025-07-24 18:34:29 +0300

    Update home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,20 +1,29 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 
-export default function Example() {
+export default function Page() {
   const [quotes, setQuotes] = useState([]);
-  useEffect(() => {
-    const fetchQuotes = async () => {
-      const responce = await fetch(
-        'http://localhost:3000/quotes/random?limit=9'
-      );
-      const data = await responce.json();
-      setQuotes(data);
-    };
+  const quotesRef = useRef(null);
+  const doScroll = useRef(false);
 
-    fetchQuotes();
-  }, []);
+  const fetchQuotes = async (e) => {
+    e.preventDefault();
+    const responce = await fetch(
+      'http://localhost:3000/quotes/random?limit=12'
+    );
+    const data = await responce.json();
+    setQuotes(data);
+    doScroll.current = true;
+  };
+
+  useEffect(() => {
+    if (doScroll.current && Array.isArray(quotes)) {
+      quotesRef.current?.scrollIntoView({ behavior: 'smooth' });
+      doScroll.current = false;
+    }
+  }, [quotes]);
 
   const welcome = (
     <>
@@ -24,7 +33,7 @@ export default function Example() {
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
               Quotes for Every Occasion
             </h1>
-            {Array.isArray(quotes) && quotes.length > 0 ? (
+            {1 ? (
               <>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
                   Discover a vast collection of over 500,000 quotes, ranging
@@ -37,12 +46,17 @@ export default function Example() {
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    href="#quotes"
+                    href="#"
+                    onClick={fetchQuotes}
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get started
+                    Get random qutes
                   </a>
-                  <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                  <a
+                    href="https://github.com/t1mm1/app-quotes"
+                    target="_blank"
+                    className="text-sm/6 font-semibold text-gray-900"
+                  >
                     Learn more <span aria-hidden="true">→</span>
                   </a>
                 </div>
@@ -50,7 +64,7 @@ export default function Example() {
             ) : (
               <>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-                  Something going wrong...
+                  Loading...
                 </p>
               </>
             )}
@@ -65,20 +79,20 @@ export default function Example() {
       {Array.isArray(quotes) && quotes.length > 0 && (
         <div
           className="p-4 max-w-7xl w-full mx-auto sm:px-6 lg:px-8"
+          ref={quotesRef}
           id="quotes"
         >
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {quotes.map((quote) => (
               <div
                 key={quote.id}
-                className="p-4 border-1 border-gray-100 rounded-sm bg-[#fcfcfc]"
+                className="p-4 border-1 border-gray-100 rounded-sm bg-[#fafafa]"
               >
                 <p className="mb-4 text-sm">"{quote.text}"</p>
                 <p className="text-left text-sm">{quote.author}</p>
                 <div className="flex flex-wrap mt-2">
                   {quote.categories.map((category) => (
                     <span
-                      href="#"
                       key={category}
                       className="text-xs bg-gray-100 text-gray-700 px-2 py-1 rounded-sm mr-2 mb-2 hover:bg-gray-400 transition-colors duration-200 cursor-default"
                     >
